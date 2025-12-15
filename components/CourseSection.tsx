d0d178c8370d71aadafa7c652b170f768e549ec6
Giuseppe <cresctelli@gmail.com>
2025-12-15 07:18:00 +0100

    Customized section info.

--- components/CourseSection.tsx
+++ components/CourseSection.tsx
@@ -1,8 +1,5 @@
 import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 
-import { ColorIcon } from "@/components/ColorIcon";
-import { Clock } from "lucide-react";
-
 interface Props {
   section: string;
 }
@@ -13,10 +10,7 @@ function CourseSection({ section }: Props) {
       <AccordionTrigger>
         <div className="flex flex-col items-start gap-2">
           <span>Section {section}: How to bulk</span>
-          <span className="flex items-center gap-2">
-            <ColorIcon icon={Clock} />
-            10 mins
-          </span>
+          <span className="flex items-center gap-2 text-muted-foreground">4/11 | 10 mins</span>
         </div>
       </AccordionTrigger>
       <AccordionContent>Contenuto</AccordionContent>
