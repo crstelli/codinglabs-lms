4aac94c2c524104e044b78eb83bb9af520c485c6
Giuseppe <cresctelli@gmail.com>
2025-12-15 07:28:39 +0100

    Fixed padding in layout.

--- components/CourseLesson.tsx
+++ components/CourseLesson.tsx
@@ -2,7 +2,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 function CourseLesson() {
   return (
-    <div className="grid grid-cols-[auto_1fr] gap-x-4 cursor-pointer py-3 hover:bg-background">
+    <div className="grid grid-cols-[auto_1fr] gap-x-4 cursor-pointer py-3 hover:bg-background px-4">
       <Checkbox className="row-span-2 size-5 my-auto" />
       <span>Lesson 1: Calories</span>
       <span className="text-muted-foreground">4 min</span>
--- components/CourseSection.tsx
+++ components/CourseSection.tsx
@@ -7,14 +7,14 @@ interface Props {
 
 function CourseSection({ section }: Props) {
   return (
-    <AccordionItem value={section} className="bg-popover px-4">
-      <AccordionTrigger>
+    <AccordionItem value={section} className="bg-popover">
+      <AccordionTrigger className="px-4">
         <div className="flex flex-col items-start gap-2">
           <span>Section {section}: How to bulk</span>
           <span className="flex items-center gap-2 text-muted-foreground">4/11 | 10 mins</span>
         </div>
       </AccordionTrigger>
-      <AccordionContent className="flex flex-col divide-y">
+      <AccordionContent className="flex flex-col divide-y py-0">
         <CourseLesson />
         <CourseLesson />
         <CourseLesson />
