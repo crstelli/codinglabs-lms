a86c74f36676ad0f54156be8d68ff2f73a3bf155
Giuseppe <cresctelli@gmail.com>
2025-12-15 07:12:34 +0100

    Customized CourseSection with props implemented reusability.

--- app/(app)/course/[courseId]/page.tsx
+++ app/(app)/course/[courseId]/page.tsx
@@ -1,4 +1,5 @@
 import { ColorIcon } from "@/components/ColorIcon";
+import { CourseSection } from "@/components/CourseSection";
 import { H2 } from "@/components/Typography/H2";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Checkbox } from "@/components/ui/checkbox";
@@ -13,30 +14,9 @@ export default function page() {
       <div className="row-span-2 h-screen divide overflow-y-auto overflow-x-hidden py-2">
         <H2>Course Content</H2>
         <Accordion type="multiple">
-          <AccordionItem value="lesson-1" className="bg-popover px-4">
-            <AccordionTrigger>
-              <div className="flex flex-col items-start gap-2">
-                <span>Section 1: How to bulk</span>
-                <span className="flex items-center gap-2">
-                  <ColorIcon icon={Clock} />
-                  10 mins
-                </span>
-              </div>
-            </AccordionTrigger>
-            <AccordionContent>Contenuto</AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="lesson-2">
-            <AccordionTrigger>
-              <div>Section 1: How to bulk</div>
-            </AccordionTrigger>
-            <AccordionContent>Contenuto</AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="lesson-3">
-            <AccordionTrigger>
-              <div>Section 1: How to bulk</div>
-            </AccordionTrigger>
-            <AccordionContent>Contenuto</AccordionContent>
-          </AccordionItem>
+          <CourseSection section="1" />
+          <CourseSection section="2" />
+          <CourseSection section="3" />
         </Accordion>
       </div>
       <div className="text-center">Course Info</div>
--- components/CourseSection.tsx
+++ components/CourseSection.tsx
@@ -3,12 +3,16 @@ import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/
 import { ColorIcon } from "@/components/ColorIcon";
 import { Clock } from "lucide-react";
 
-function CourseSection() {
+interface Props {
+  section: string;
+}
+
+function CourseSection({ section }: Props) {
   return (
-    <AccordionItem value="lesson-1" className="bg-popover px-4">
+    <AccordionItem value={section} className="bg-popover px-4">
       <AccordionTrigger>
         <div className="flex flex-col items-start gap-2">
-          <span>Section 1: How to bulk</span>
+          <span>Section {section}: How to bulk</span>
           <span className="flex items-center gap-2">
             <ColorIcon icon={Clock} />
             10 mins
