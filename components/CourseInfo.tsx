0168e459d730ca7e681427b1d78230688331fdb8
Giuseppe <cresctelli@gmail.com>
2025-12-16 07:24:34 +0100

    Added ratings to course info.

--- components/CourseInfo.tsx
+++ components/CourseInfo.tsx
@@ -1,5 +1,7 @@
 import { H1 } from "@/components/Typography/H1";
+import { P } from "@/components/Typography/P";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Star } from "lucide-react";
 
 function CourseInfo() {
   return (
@@ -12,6 +14,13 @@ function CourseInfo() {
         </TabsList>
         <TabsContent value="introduction">
           <H1>How to builk and get an aesthetic body</H1>
+          <div className="flex items-center gap-4">
+            <span className="flex items-center gap-1 text-yellow-500">
+              <Star size={20} />
+              <span>4.6</span>
+            </span>
+            <P>by Giuseppe Crescitelli</P>
+          </div>
         </TabsContent>
       </Tabs>
     </div>
