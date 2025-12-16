ad76c9ecff374f0845b01fcb6e18d917f66f12b8
Giuseppe <cresctelli@gmail.com>
2025-12-16 07:20:37 +0100

    Built tabs component improving the layout.

--- components/CourseInfo.tsx
+++ components/CourseInfo.tsx
@@ -1,17 +1,20 @@
 import { H1 } from "@/components/Typography/H1";
-import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function CourseInfo() {
-  // return <H1>How to builk and get an aesthetic body</H1>;
-
   return (
-    <Tabs>
-      <TabsList>
-        <TabsTrigger value="introduction">Introduction</TabsTrigger>
-        <TabsTrigger value="questions">Questions</TabsTrigger>
-        <TabsTrigger value="reviews">Reviews</TabsTrigger>
-      </TabsList>
-    </Tabs>
+    <div className="p-6">
+      <Tabs className="gap-6" defaultValue="introduction">
+        <TabsList>
+          <TabsTrigger value="introduction">Introduction</TabsTrigger>
+          <TabsTrigger value="questions">Questions</TabsTrigger>
+          <TabsTrigger value="reviews">Reviews</TabsTrigger>
+        </TabsList>
+        <TabsContent value="introduction">
+          <H1>How to builk and get an aesthetic body</H1>
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 }
 
