71104b3910fe80a481013c3aeae1f4ffc1e7b657
Giuseppe <cresctelli@gmail.com>
2025-12-16 07:26:34 +0100

    Added infos to course page.

--- components/CourseInfo.tsx
+++ components/CourseInfo.tsx
@@ -1,7 +1,7 @@
 import { H1 } from "@/components/Typography/H1";
 import { P } from "@/components/Typography/P";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Star } from "lucide-react";
+import { Clock, Star, User } from "lucide-react";
 
 function CourseInfo() {
   return (
@@ -13,11 +13,21 @@ function CourseInfo() {
           <TabsTrigger value="reviews">Reviews</TabsTrigger>
         </TabsList>
         <TabsContent value="introduction">
-          <H1>How to builk and get an aesthetic body</H1>
-          <div className="flex items-center gap-4">
+          <H1>How to bulk and get an aesthetic body</H1>
+          <div className="flex items-center gap-6">
             <span className="flex items-center gap-1 text-yellow-500">
               <Star size={20} />
               <span>4.6</span>
+            </span>
+
+            <span className="flex items-center gap-1">
+              <User size={20} />
+              <span>14.231</span>
+            </span>
+
+            <span className="flex items-center gap-1">
+              <Clock size={20} />
+              <span>10.5h</span>
             </span>
             <P>by Giuseppe Crescitelli</P>
           </div>
