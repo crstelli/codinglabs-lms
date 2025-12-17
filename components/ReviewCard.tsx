00035d80f098adb57b1a35665e5054207b437c3a
Giuseppe <cresctelli@gmail.com>
2025-12-17 07:47:52 +0100

    Fixed props order.

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -8,8 +8,8 @@ function ReviewCard() {
       date="7 months ago"
       user={{ avatar: "/avatar-placeholder.png", name: "Spocchimax" }}
       body={{
-        content: "Never seen a course like this, incredible.",
         title: "Best course on the platform",
+        content: "Never seen a course like this, incredible.",
         subtitleNode: <StarRating rating={5} className="mt-2" />,
       }}
     />
