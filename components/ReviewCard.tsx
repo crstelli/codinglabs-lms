9a1760f668c597a98c336c2aaac12a0eb9e11a43
Giuseppe <cresctelli@gmail.com>
2025-12-17 07:45:53 +0100

    Rebuild ReviewCard using the ContentCard component.

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -1,37 +1,18 @@
+import { ContentCard } from "@/components/ContentCard";
 import { StarRating } from "@/components/StarRating";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { CircleChevronUp } from "lucide-react";
 
 function ReviewCard() {
   return (
-    <Card className="grid grid-cols-[auto_1fr_auto] max-w-2xl">
-      <CardHeader>
-        <Avatar>
-          <AvatarImage src="/avatar-placeholder.png" alt="@evilrabbit" />
-          <AvatarFallback>ER</AvatarFallback>
-        </Avatar>
-      </CardHeader>
-      <CardContent className="flex flex-col px-2">
-        <CardTitle>Best course on the platform</CardTitle>
-        <StarRating className="py-2" rating={5} />
-        <CardDescription>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ipsam assumenda hic similique minima quidem.
-        </CardDescription>
-
-        <div className="mt-4 text-sm flex items-center gap-4">
-          <span className="text-primary">User 21</span>
-          <span>1 year ago</span>
-        </div>
-      </CardContent>
-      <CardFooter className="flex flex-col items-end justify-between">
-        <Button size={"icon-sm"}>
-          <CircleChevronUp />
-        </Button>
-        <span>351 votes</span>
-      </CardFooter>
-    </Card>
+    <ContentCard
+      votes={208}
+      date="7 months ago"
+      user={{ avatar: "/avatar-placeholder.png", name: "Spocchimax" }}
+      body={{
+        content: "Never seen a course like this, incredible.",
+        title: "Best course on the platform",
+        subtitleNode: <StarRating rating={5} className="mt-2" />,
+      }}
+    />
   );
 }
 
