afb665338e7c11011c083eada05f98db611abc35
Giuseppe <cresctelli@gmail.com>
2025-12-17 07:21:32 +0100

    Built TabReviews with the same structure of other course sections.

--- components/TabReviews.tsx
+++ components/TabReviews.tsx
@@ -5,7 +5,7 @@ import { ReviewCard } from "@/components/ReviewCard";
 function TabReviews() {
   return (
     <TabsContent value="reviews">
-      <H1>Questions about this course</H1>
+      <H1>Reviews of this course</H1>
       <div className="grid grid-cols-2 gap-4">
         <ReviewCard />
         <ReviewCard />
