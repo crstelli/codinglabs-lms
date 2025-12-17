44de95e2bd39aa9fc74a2a23a23c16428d1aee55
Giuseppe <cresctelli@gmail.com>
2025-12-17 07:48:12 +0100

    Refactoring: rebuild the QuestionCard component with reusable ContentCard component.

--- components/QuestionCard.tsx
+++ components/QuestionCard.tsx
@@ -1,35 +1,16 @@
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { CircleChevronUp } from "lucide-react";
+import { ContentCard } from "@/components/ContentCard";
 
 function QuestionCard() {
   return (
-    <Card className="grid grid-cols-[auto_1fr_auto] max-w-2xl">
-      <CardHeader>
-        <Avatar>
-          <AvatarImage src="/avatar-placeholder.png" alt="@evilrabbit" />
-          <AvatarFallback>ER</AvatarFallback>
-        </Avatar>
-      </CardHeader>
-      <CardContent className="flex flex-col px-2">
-        <CardTitle>How to use MyFitnessPal?</CardTitle>
-        <CardDescription className="mt-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ipsam assumenda hic similique minima quidem.
-        </CardDescription>
-
-        <div className="mt-4 text-sm flex items-center gap-4">
-          <span className="text-primary">User 21</span>
-          <span>1 year ago</span>
-        </div>
-      </CardContent>
-      <CardFooter className="flex flex-col items-end justify-between">
-        <Button size={"icon-sm"}>
-          <CircleChevronUp />
-        </Button>
-        <span>351 votes</span>
-      </CardFooter>
-    </Card>
+    <ContentCard
+      votes={104}
+      date="4 months ago"
+      user={{ avatar: "/avatar-placeholder.png", name: "Spocchimax" }}
+      body={{
+        title: "My personal question.",
+        content: "Should I builk or should I cut?",
+      }}
+    />
   );
 }
 
